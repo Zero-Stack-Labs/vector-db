eab931e60a6695aa6fce390fd32108537daf0571
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-20 23:04:16 -0500

    eliminate comments

--- app/services/vector_db_service_interface.py
+++ app/services/vector_db_service_interface.py
@@ -5,21 +5,12 @@ from app.models import IndexConfig, UpsertRequest, QueryRequest
 class VectorDBServiceInterface(ABC):
     @abstractmethod
     def create_index(self, provider_name: str, config: IndexConfig):
-        """
-        Método para crear un índice en el proveedor de base de datos vectorial.
-        """
         pass
 
     @abstractmethod
     def upsert_data(self, provider_name: str, index_name: str, upsert_request: UpsertRequest):
-        """
-        Método para insertar o actualizar datos en el índice de la base de datos vectorial.
-        """
         pass
 
     @abstractmethod
     def search(self, provider_name: str, index_name: str, query_request: QueryRequest):
-        """
-        Método para realizar una búsqueda en el índice de la base de datos vectorial.
-        """
         pass
